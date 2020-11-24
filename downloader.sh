75da4a475c337410a0e29d226c4f49c34bbf5099
boyesr1 <richard.boyes@centrica.com>
2020-11-24 12:33:06 +0000

    spelling mistake

--- downloader.sh
+++ downloader.sh
@@ -7,7 +7,7 @@ DT=$(date "+%Y-%m-%d")
 ENV_PATH=/volume1/share/venv/covid19/bin/activate
 PYTHON_PATH="$(dirname $(realpath $0))/downloader.py"
 CSV_PATH="/volume1/web/covid19-data/covid19-$DT.csv"
-LOG_PATH="/temp/covid19-$DT.log"
+LOG_PATH="/tmp/covid19-$DT.log"
 
 . $ENV_PATH
 python $PYTHON_PATH $CSV_PATH $LOG_PATH
