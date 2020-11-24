99b8c16b30d4be051af6bd087ba647437d757c0f
boyesr1 <richard.boyes@centrica.com>
2020-11-24 12:24:16 +0000

    updates

--- downloader.sh
+++ downloader.sh
@@ -5,9 +5,9 @@ set -u
 DT=$(date "+%Y-%m-%d")
 
 ENV_PATH=/volume1/share/venv/covid19/bin/activate
-PYTHON_PATH=/volume1/share/web
-CSV_PATH="/volume1/web/covid19/covid19-"$DT".csv"
-LOG_PATH="/temp/covid19-"$DT".log"
+PYTHON_PATH="$(dirname $(realpath $0))/downloader.py"
+CSV_PATH="/volume1/web/covid19-data/covid19-$DT.csv"
+LOG_PATH="/temp/covid19-$DT.log"
 
 . $ENV_PATH
 python $PYTHON_PATH $CSV_PATH $LOG_PATH
