11d24e5b385ebdf53cb8efb9fe2ac8e1befd103b
boyesr1 <richard.boyes@centrica.com>
2020-12-10 15:38:46 +0000

    rejigging pipenv use in shell

--- downloader.sh
+++ downloader.sh
@@ -4,14 +4,16 @@ set -u
 #Date of processing
 DT=$(date "+%Y-%m-%d")
 
-python3 -m pipenv install
-
 CSV_DIR='/volume1/web/covid19-data'
 LOG_DIR='/tmp'
 
 CSV_PATH="$CSV_DIR/covid19-$DT.csv"
 LOG_PATH="$LOG_DIR/covid19-$DT.log"
-python3 -m pipenv run python "$(dirname $(realpath $0))/downloader.py $CSV_PATH $LOG_PATH"
+
+THIS_DIR="$(dirname $(realpath $0))"
+cd $THIS_DIR && 
+  python3 -m pipenv install &&
+  python3 -m pipenv run python "$THIS_DIR/downloader.py $CSV_PATH $LOG_PATH"
 
 find $CSV_DIR -type f -mtime +7 -name 'covid19*.csv' | xargs rm -f
 find $LOG_DIR -type f -mtime +7 -name 'covid19*.log' | xargs rm -f
