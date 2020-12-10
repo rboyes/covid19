7411ebbf64e976102c66e3234206cdd434b85917
boyesr1 <richard.boyes@centrica.com>
2020-12-10 15:31:50 +0000

    Using pipenv to manage environments, adding a cleanup

--- downloader.sh
+++ downloader.sh
@@ -4,12 +4,15 @@ set -u
 #Date of processing
 DT=$(date "+%Y-%m-%d")
 
-ENV_PATH=/volume1/share/venv/covid19/bin/activate
-PYTHON_PATH="$(dirname $(realpath $0))/downloader.py"
-CSV_PATH="/volume1/web/covid19-data/covid19-$DT.csv"
-LOG_PATH="/tmp/covid19-$DT.log"
+python3 -m pipenv install
 
-. $ENV_PATH
-python $PYTHON_PATH $CSV_PATH $LOG_PATH
+CSV_DIR='/volume1/web/covid19-data'
+LOG_DIR='/tmp'
 
-deactivate
+CSV_PATH="$CSV_DIR/covid19-$DT.csv"
+LOG_PATH="$LOG_DIR/covid19-$DT.log"
+python3 -m pipenv run python "$(dirname $(realpath $0))/downloader.py $CSV_PATH $LOG_PATH"
+
+find $CSV_DIR -type f -mtime +7 -name 'covid19*.csv' | xargs rm -f
+find $LOG_DIR -type f -mtime +7 -name 'covid19*.log' | xargs rm -f
+
