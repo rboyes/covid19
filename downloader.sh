fa00e7f919a3ab0957eed479fc876cdb45eed0db
Richard Boyes <rboyes@gmail.com>
2020-12-16 16:27:49 +0000

    Compressing csv to speed up data retrieval

--- downloader.sh
+++ downloader.sh
@@ -13,8 +13,9 @@ LOG_PATH="$LOG_DIR/covid19-$DT.log"
 THIS_DIR="$(dirname $(realpath $0))"
 cd $THIS_DIR && 
   python3 -m pipenv install &&
-  python3 -m pipenv run python "$THIS_DIR/downloader.py" $CSV_PATH $LOG_PATH
+  python3 -m pipenv run python "$THIS_DIR/downloader.py" $CSV_PATH $LOG_PATH &&
+  gzip $CSV_PATH
 
-find $CSV_DIR -type f -mtime +7 -name 'covid19*.csv' | xargs rm -f
+find $CSV_DIR -type f -mtime +7 -name 'covid19*.csv*' | xargs rm -f
 find $LOG_DIR -type f -mtime +7 -name 'covid19*.log' | xargs rm -f
 
